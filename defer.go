fe5293e6e5db5dbc119276ab0e686c8f51570cac
Renjith <renjith@sayonetech.com>
2018-05-05 12:07:27 +0530

    differ dcoument added

--- defer.go
+++ defer.go
@@ -4,6 +4,11 @@ import "fmt"
 
 //https://blog.golang.org/defer-panic-and-recover
 func defersample() {
+
+	//A defer statement defers the execution of a function until the surrounding function returns.
+
+	//The deferred call's arguments are evaluated immediately,
+	//but the function call is not executed until the surrounding function returns.
 	defer fmt.Println("world")
 
 	fmt.Println("hello")
